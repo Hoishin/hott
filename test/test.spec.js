8bb279c5351ac1d14343a60fb31c53a0b0a4a1f8
Alex Van Camp <vancamp.alex@gmail.com>
2016-12-27 22:10:07 -0600

    add tests and appveyor.yml

---
+++ test/test.spec.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const hott = require('../index');
+const robot = require('robotjs');
+
+describe('hott', () => {
+	before(() => {
+		hott.monitorHotkeys();
+	});
+
+	it('should detect keys with no modifiers', done => {
+		hott.registerHotkey('VK_KEY_X', [], () => {
+			done();
+		});
+
+		robot.keyTap('x');
+	});
+
+	it('should detect keys with one modifier', done => {
+		hott.registerHotkey('VK_KEY_X', ['MOD_SHIFT'], () => {
+			done();
+		});
+
+		robot.keyTap('x', 'shift');
+	});
+
+	it('should detect keys with multiple modifiers', done => {
+		hott.registerHotkey('VK_KEY_X', ['MOD_SHIFT', 'MOD_CONTROL'], () => {
+			done();
+		});
+
+		robot.keyTap('x', ['shift', 'control']);
+	});
+});
