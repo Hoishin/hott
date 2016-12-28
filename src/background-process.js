385c7e69beb46618ca98658a0e8dc22b205de507
Alex Van Camp <vancamp.alex@gmail.com>
2016-12-27 19:13:27 -0600

    strip down to essentials. make it work on node 6. eslint-config-xo.

--- src/background-process.js
+++
@@ -1,7 +0,0 @@
-var $ = require('get-me')(require, {
-    hott: '../index.js'
-});
-
-$.suspend($.hott)({
-    config: $.path.resolve(process.env.USERPROFILE, '.hott')
-});
--- test/app.js
+++ test/app.js
@@ -1,24 +1,19 @@
-var $ = require('get-me')(require, {
-    hott: '../index.js'
+const hott = require('../index');
+
+hott.registerHotkey('VK_KEY_G', ['MOD_SHIFT'], () => {
+	console.log('Shift+G!');
 });
 
-$.hott.Api.registerHotkey("VK_KEY_G", ["MOD_SHIFT"], function onShiftEightPressed() {
-    console.log('Shift+G!');
-});
-$.hott.Api.registerHotkey("VK_UP", ["MOD_SHIFT", "MOD_ALT", "MOD_CONTROL"], {
-    js: "console.log('Alt+Ctrl+Shift+Up!');"
-});
-
-var end = $.hott.Api.monitorHotkeys();
+const end = hott.monitorHotkeys();
 console.log('Now monitoring!');
 
-var i = 5;
-var self = setInterval(function() {
-    if(!i--) {
-        end();
-        clearInterval(self);
-        console.log('fin');
-    } else {
-        console.log('tick');
-    }
+let i = 5;
+const self = setInterval(() => {
+	if (i--) {
+		console.log('tick');
+	} else {
+		end();
+		clearInterval(self);
+		console.log('fin');
+	}
 }, 1000);
