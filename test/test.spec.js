a085ec0d2a7d69c7896085a0e6b074fdac150317
Keiichiro Amemiya <hoishinxii@gmail.com>
2020-10-03 03:21:37 +0900

    test: add delay to make tests consistent

--- test/test.spec.js
+++ test/test.spec.js
@@ -2,37 +2,46 @@
 
 const hott = require('../index');
 const robot = require('robotjs');
+const delay = require('delay');
 
+const ASYNC_DELAY = 300;
 let clearMonitor;
 
-beforeAll(() => {
+beforeAll(async () => {
 	clearMonitor = hott.monitorHotkeys();
+	await delay(ASYNC_DELAY);
 });
 
 afterAll(() => {
 	clearMonitor();
 });
 
-test('should detect keys with no modifiers', done => {
+test('should detect keys with no modifiers', async done => {
 	hott.registerHotkey('VK_KEY_X', [], () => {
 		done();
 	});
 
+	await delay(ASYNC_DELAY);
+
 	robot.keyTap('x');
 });
 
-test('should detect keys with one modifier', done => {
-	hott.registerHotkey('VK_KEY_X', ['MOD_SHIFT'], () => {
+test('should detect keys with one modifier', async done => {
+	hott.registerHotkey('VK_KEY_Y', ['MOD_SHIFT'], () => {
 		done();
 	});
 
-	robot.keyTap('x', 'shift');
+	await delay(ASYNC_DELAY);
+
+	robot.keyTap('y', 'shift');
 });
 
-test('should detect keys with multiple modifiers', done => {
-	hott.registerHotkey('VK_KEY_X', ['MOD_SHIFT', 'MOD_CONTROL'], () => {
+test('should detect keys with multiple modifiers', async done => {
+	hott.registerHotkey('VK_KEY_Z', ['MOD_SHIFT', 'MOD_CONTROL'], () => {
 		done();
 	});
 
-	robot.keyTap('x', ['shift', 'control']);
+	await delay(ASYNC_DELAY);
+
+	robot.keyTap('z', ['shift', 'control']);
 });
